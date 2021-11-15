ba6fc422bd99509c40f08a31fd68a0430f4425ca
Tanner Gates <tgates4@dmail.dixie.edu>
2021-11-14 22:15:57 -0700

    first commit

---
+++ app/Main.js
@@ -0,0 +1,44 @@
+import React, { useState } from "react"
+import ReactDOM from "react-dom"
+import { BrowserRouter, Switch, Route } from "react-router-dom"
+import Axios from "axios"
+Axios.defaults.baseURL = "http://localhost:8080"
+
+//Components
+import Header from "./components/Header"
+import HomeGuest from "./components/HomeGuest"
+import Footer from "./components/Footer"
+import About from "./components/About"
+import Terms from "./components/Terms"
+import Home from "./components/Home"
+import CreatePost from "./components/CreatePost"
+
+function Main() {
+  const [loggedIn, setLoggedIn] = useState(Boolean(localStorage.getItem("complexappToken")))
+  return (
+    <BrowserRouter>
+      <Header loggedIn={loggedIn} setLoggedIn={setLoggedIn} />
+      <Switch>
+        <Route path="/" exact>
+          {loggedIn ? <Home /> : <HomeGuest />}
+        </Route>
+        <Route path="/create-post">
+          <CreatePost />
+        </Route>
+        <Route path="/about-us">
+          <About />
+        </Route>
+        <Route path="/terms">
+          <Terms />
+        </Route>
+      </Switch>
+      <Footer />
+    </BrowserRouter>
+  )
+}
+
+ReactDOM.render(<Main />, document.querySelector("#app"))
+
+if (module.hot) {
+  module.hot.accept()
+}
