239d3a1e68f69b12f49e34fdbc7c7c5cb322538f
Tanner Gates <tgates4@dmail.dixie.edu>
2021-11-18 19:02:27 -0700

    Add Profile component

--- app/Main.js
+++ app/Main.js
@@ -17,6 +17,7 @@ import Home from "./components/Home"
 import CreatePost from "./components/CreatePost"
 import ViewSinglePost from "./components/ViewSinglePost"
 import FlashMessages from "./components/FlashMessages"
+import Profile from "./components/Profile"
 
 function Main() {
   const initialState = {
@@ -63,6 +64,9 @@ function Main() {
           <FlashMessages messages={state.flashMessages} />
           <Header />
           <Switch>
+            <Route path="/profile/:username">
+              <Profile />
+            </Route>
             <Route path="/" exact>
               {state.loggedIn ? <Home /> : <HomeGuest />}
             </Route>
