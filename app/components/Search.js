3c6d42431a5cf4943be0759707d96bc0870d34c8
Tanner Gates <tgates4@dmail.dixie.edu>
2021-11-25 09:09:04 -0700

    Set up Search handler

--- app/components/Search.js
+++ app/components/Search.js
@@ -1,14 +1,37 @@
 import React, { useEffect, useContext } from "react"
 import DispatchContext from "../DispatchContext"
+import { useImmer } from "use-immer"
 
 function Search() {
   const appDispatch = useContext(DispatchContext)
+  const [state, setState] = useImmer({
+    searchTerm: "",
+    results: [],
+    show: "neither",
+    requestCount: 0
+  })
 
   useEffect(() => {
     document.addEventListener("keyup", searchKeyPressHandler)
 
     return () => document.removeEventListener("keyup", searchKeyPressHandler)
   }, [])
+
+  useEffect(() => {
+    const delay = setTimeout(() => {
+      setState(draft => {
+        draft.requestCount++
+      })
+    }, 3000)
+
+    return () => clearTimeout(delay)
+  }, [state.searchTerm])
+
+  useEffect(() => {
+    if (state.requestCount) {
+      // Send axios request here
+    }
+  }, [state.requestCount])
 
   function searchKeyPressHandler(e) {
     if (e.keyCode == 27) {
@@ -19,6 +42,13 @@ function Search() {
   function handleCloseSearch(e) {
     e.preventDefault()
   }
+
+  function handleInput(e) {
+    const value = e.target.value
+    setState(draft => {
+      draft.searchTerm = value
+    })
+  }
   return (
     <div className="search-overlay">
       <div className="search-overlay-top shadow-sm">
@@ -26,7 +56,7 @@ function Search() {
           <label htmlFor="live-search-field" className="search-overlay-icon">
             <i className="fas fa-search"></i>
           </label>
-          <input autoFocus type="text" autoComplete="off" id="live-search-field" className="live-search-field" placeholder="What are you interested in?" />
+          <input onChange={handleInput} autoFocus type="text" autoComplete="off" id="live-search-field" className="live-search-field" placeholder="What are you interested in?" />
           <span onClick={() => appDispatch({ type: "closeSearch" })} className="close-live-search">
             <i className="fas fa-times-circle"></i>
           </span>
